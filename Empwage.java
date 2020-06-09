4b6c770abf68b7879da8f04efd4a552efb53d092
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-06-09 20:30:45 +0530

    ADDED: UC4 functionality added, switch case functionality added

--- Empwage.java
+++ Empwage.java
@@ -10,21 +10,24 @@ public class Empwage
 		int HOUR_PER_DAY_PART=4;
 		//computation
 		double empcheck = Math.floor(Math.random() * 10) % 2;
-		if ( empcheck == IS_PRESENT)
+		if ( empcheck == IS_PRESENT )
 		{
 			System.out.println("Employee is Present");
 			double job_type = Math.floor(Math.random() * 10) % 2;
-			if ( job_type == 1 )
+			switch((int)job_type)
 			{
-				System.out.println("Employee works Full Time");
-				int dailywage = (WAGE_PER_HOUR * HOUR_PER_DAY_FULL);
-				System.out.println("Daily wage would be = Rs." + dailywage);
-			}
-			else
-			{
-            System.out.println("Employee works Part Time");
-            int dailywagep = (WAGE_PER_HOUR * HOUR_PER_DAY_PART);
-            System.out.println("Daily wage would be = Rs." + dailywagep);
+				case 1:
+					System.out.println("Employee works Full Time");
+					int dailywage = (WAGE_PER_HOUR * HOUR_PER_DAY_FULL);
+					System.out.println("Daily wage would be = Rs." + dailywage);
+					break;
+				case 0:
+            	System.out.println("Employee works Part Time");
+            	int dailywagep = (WAGE_PER_HOUR * HOUR_PER_DAY_PART);
+            	System.out.println("Daily wage would be = Rs." + dailywagep);
+					break;
+				default:
+					System.out.println("Invalid");
 			}
 
 		}
