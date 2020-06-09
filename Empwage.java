bf4d141303d494d98d9ebd3a19c390deb0f0d88d
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-06-09 20:01:04 +0530

    ADDED: UC2 functionality added, daily wage calculated

--- Empwage.java
+++ Empwage.java
@@ -5,14 +5,21 @@ public class Empwage
 		System.out.println("Welcome To Employee Wage Computation Process");
 		//constants
 		int IS_FULL_TIME=1;
+		int WAGE_PER_HOUR=20;
+		int HOUR_PER_DAY=8;
 		//computation
 		double empcheck = Math.floor(Math.random() * 10) % 2;
 		if ( empcheck == IS_FULL_TIME)
+		{
 			System.out.println("Employee is Present");
+			int dailywage = (WAGE_PER_HOUR * HOUR_PER_DAY);
+			System.out.println("Daily wage would be = Rs." + dailywage);
+		}
 		else
 			System.out.println("Employee is Absent");
 	}
 
 }
+
 
 
