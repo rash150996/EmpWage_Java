2e00c83d9aa5709060c5bb7c1c3ccef248dce5e7
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-06-09 20:12:44 +0530

    ADDED: UC3 functionality added, daily wage calculated for PART TIME

--- Empwage.java
+++ Empwage.java
@@ -4,16 +4,29 @@ public class Empwage
 	{
 		System.out.println("Welcome To Employee Wage Computation Process");
 		//constants
-		int IS_FULL_TIME=1;
+		int IS_PRESENT=1;
 		int WAGE_PER_HOUR=20;
-		int HOUR_PER_DAY=8;
+		int HOUR_PER_DAY_FULL=8;
+		int HOUR_PER_DAY_PART=4;
 		//computation
 		double empcheck = Math.floor(Math.random() * 10) % 2;
-		if ( empcheck == IS_FULL_TIME)
+		if ( empcheck == IS_PRESENT)
 		{
 			System.out.println("Employee is Present");
-			int dailywage = (WAGE_PER_HOUR * HOUR_PER_DAY);
-			System.out.println("Daily wage would be = Rs." + dailywage);
+			double job_type = Math.floor(Math.random() * 10) % 2;
+			if ( job_type == 1 )
+			{
+				System.out.println("Employee works Full Time");
+				int dailywage = (WAGE_PER_HOUR * HOUR_PER_DAY_FULL);
+				System.out.println("Daily wage would be = Rs." + dailywage);
+			}
+			else
+			{
+            System.out.println("Employee works Part Time");
+            int dailywagep = (WAGE_PER_HOUR * HOUR_PER_DAY_PART);
+            System.out.println("Daily wage would be = Rs." + dailywagep);
+			}
+
 		}
 		else
 			System.out.println("Employee is Absent");
