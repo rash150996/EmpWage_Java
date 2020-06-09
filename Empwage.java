c42b38d16e0328a839d663dde0bb9adbb88a756c
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-06-09 19:53:16 +0530

    ADDED: UC1 functionality added, Employee absent or present

--- Empwage.java
+++ Empwage.java
@@ -2,7 +2,15 @@ public class Empwage
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome To Employee Wage Computation Program");
+		System.out.println("Welcome To Employee Wage Computation Process");
+		//constants
+		int IS_FULL_TIME=1;
+		//computation
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+		if ( empcheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
