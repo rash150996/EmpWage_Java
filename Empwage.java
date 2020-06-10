ad5531e58e4724e11f37fec6f2c7aefaf8a55ae3
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-06-10 16:45:27 +0530

    MODIFIED: Maximum hours to stay below 100 and conditional statement to check max hours and days a month

--- Empwage.java
+++ Empwage.java
@@ -1,46 +1,73 @@
 public class Empwage
 {
+		//constants
+	int IS_PRESENT=1;
+   int WAGE_PER_HOUR = 20;
+   int HOUR_PER_DAY_FULL = 8;
+   int HOUR_PER_DAY_PART = 4;
+   int DAYS_PER_MONTH = 20;
+   int HOURS_PER_MONTH = 100;
+
+      //variables
+   int totalEmpHrs = 0;
+   int totalWorkingDays = 0;
+   int empHrs = 0;
+   int dailyWage = 0;
+   int totalSalary = 0;
+	double jobType = 0;
+	double empcheck = 0;
+	int[] Days;
+	int[] DailyWage;
+	int[] TotalSalary;
+
 	public static void main(String[] args)
 	{
+		Empwage obj = new Empwage();
+		obj.EmpCalc();
+	}
+
+	public int EmpCalc()
+	{
 		System.out.println("Welcome To Employee Wage Computation Process");
-		//constants
-		int IS_PRESENT=1;
-		int WAGE_PER_HOUR=20;
-		int HOUR_PER_DAY_FULL=8;
-		int HOUR_PER_DAY_PART=4;
-		int DAY_PER_MONTH=20;
-		//computation
-		double empcheck = Math.floor(Math.random() * 10) % 2;
+		empcheck = Math.floor(Math.random() * 10) % 2;
 		if ( empcheck == IS_PRESENT )
 		{
 			System.out.println("Employee is Present");
-			double job_type = Math.floor(Math.random() * 10) % 2;
-			switch((int)job_type)
+			jobType = Math.floor(Math.random() * 10) % 2;
+			switch((int)jobType)
 			{
 				case 1:
-					System.out.println("Employee works Full Time");
-					int dailywage = (WAGE_PER_HOUR * HOUR_PER_DAY_FULL);
-					System.out.println("Daily wage would be = Rs." + dailywage);
-					int monthlywage = (dailywage * DAY_PER_MONTH );
-					System.out.println("Monthly wage would be = Rs." + monthlywage );
+					 empHrs=8;
 					break;
 				case 0:
-					System.out.println("Employee works Part Time");
-					int dailywagep = (WAGE_PER_HOUR * HOUR_PER_DAY_PART);
-					System.out.println("Daily wage would be = Rs." + dailywagep);
-					int monthlywagep = (dailywagep * DAY_PER_MONTH );
-					System.out.println("Monthly wage would be = Rs." + monthlywagep );
+					 empHrs=4;
 					break;
 				default:
-					System.out.println("Invalid");
+					 empHrs=0;
+					break;
 			}
 
 		}
 		else
 			System.out.println("Employee is Absent");
-	}
+
+		while ( totalWorkingDays <= DAYS_PER_MONTH && totalEmpHrs <= HOURS_PER_MONTH )
+		{
+			totalWorkingDays+=1;
+			if ( (totalEmpHrs + empHrs) < HOURS_PER_MONTH )
+			{
+				totalEmpHrs+=empHrs;
+			}
+			else
+				break;
+			dailyWage = (empHrs * WAGE_PER_HOUR);
+		}
+	System.out.println("Total Working Hours = " + totalEmpHrs );
+	totalSalary = (totalEmpHrs * WAGE_PER_HOUR);
+	System.out.println("Total salary = " + totalSalary);
+	return totalSalary;
+ }
 
 }
-
 
 
